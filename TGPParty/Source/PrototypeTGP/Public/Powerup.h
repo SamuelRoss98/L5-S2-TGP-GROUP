0a122f6752c90150c40fd1c8af1f7c64dbd5ce04
Sam Johnson <sam.ross.johnson@gmail.com>
2019-02-10 10:20:41 +0000

    Aded Powerup struct

---
+++ TGPParty/Source/PrototypeTGP/Public/Powerup.h
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#pragma once
+
+#include "Engine/DataTable.h"
+#include "StatModifier.h"
+
+#include "Powerup.generated.h"
+
+USTRUCT(BlueprintType)
+struct FPowerup : public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+
+public:
+	FPowerup() {}
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString Name;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FText IconPath;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bTeleport;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bSpawnClones;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bSpawnDecoyTreasure;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FStatModifier SpeedModifier;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FStatModifier SlownessModifier;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FStatModifier StunModifier;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FStatModifier FrictionModifier;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FStatModifier ForcePullModifier;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FStatModifier ForcePushModifier;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FStatModifier VisibilityModifier;
+};
